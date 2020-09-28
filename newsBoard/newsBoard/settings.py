4fc6f285cd559d79b8a835e4bc8fe0e1de1d5432
ostegura <stegura99@gmail.com>
2020-09-29 01:41:57 +0300

    another staticfiles fix

--- newsBoard/newsBoard/settings.py
+++ newsBoard/newsBoard/settings.py
@@ -152,6 +152,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
